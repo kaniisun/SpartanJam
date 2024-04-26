1af599474f9b0065fc28533f80196af1a8ddbd4f
sddelcampo@uncg.edu <sddelcampo@uncg.edu>
2024-04-26 14:59:19 -0400

    I worked on having the database be appended to your music but to no avail on my end I want to know if there is something im missing

--- CSC340/src/main/java/com/SpartanJam/CSC340/controller/ArtistController.java
+++ CSC340/src/main/java/com/SpartanJam/CSC340/controller/ArtistController.java
@@ -1,43 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.SpartanJam.CSC340.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- * @author sreyk
- */
 @Controller
 @RequestMapping("/artist")
 public class ArtistController {
     
     @GetMapping("/artistlogin")
     public String listenerLogin() {
-        
         return "artistlogin";
     }
     
     @GetMapping("/artistpage")
     public String artistPage() {
-        
         return "artist";
     }
         
-    @GetMapping("artist/artistedit")
+    @GetMapping("/artistedit")
     public String artistEdit() {
-
-        return "artistedit";
+        return "artist/artistedit";
     }
     
     @GetMapping("/artistupload")
     public String artistUpload() {
-        
         return "artist/artistupload";
     }
-    
 }
