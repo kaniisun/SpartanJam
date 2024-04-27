749b3be0a03d6c6719848e6df987cdf3abe7a7a5
unknown <sddelcampo@uncg.edu>
2024-04-27 10:34:45 -0400

    Tried working on the appending list to Your Music such as by adding to SongController and changing the artist.html but still no gains I get an empty Your Music list I feel like it may be that its not getting the right tableor that the backend is not working right or something

--- CSC340/src/main/java/com/SpartanJam/CSC340/controller/ArtistController.java
+++ CSC340/src/main/java/com/SpartanJam/CSC340/controller/ArtistController.java
@@ -1,30 +1,46 @@
 package com.SpartanJam.CSC340.controller;
 
+import com.SpartanJam.CSC340.model.ArtistSong;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.security.core.Authentication;
+
+import com.SpartanJam.CSC340.repository.SongRepository; // Assuming the package location is correct
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 @RequestMapping("/artist")
 public class ArtistController {
-    
+
+    @Autowired
+    private SongRepository songRepository;
+
     @GetMapping("/artistlogin")
     public String listenerLogin() {
         return "artistlogin";
     }
-    
+
     @GetMapping("/artistpage")
-    public String artistPage() {
+    public String artistPage(Model model, Authentication authentication) {
+        System.out.println("artistPage is nowo running");
+        String username = authentication.getName();
+        System.out.println("Username: " + username);
+        List<ArtistSong> songs = songRepository.findByUploaderUsername(username); // Assuming this method exists
+        System.out.println("Number of songs retrieved: " + songs.size());
+        model.addAttribute("songs", songs);
         return "artist";
     }
-        
-    @GetMapping("/artistedit")
-    public String artistEdit() {
-        return "artist/artistedit";
-    }
-    
+
     @GetMapping("/artistupload")
     public String artistUpload() {
         return "artist/artistupload";
+    }
+
+    @GetMapping("/artisedit")
+    public String artistEdit() {
+        return "artist/artisedit";
     }
 }
--- CSC340/src/main/java/com/SpartanJam/CSC340/repository/SongRepository.java
+++ CSC340/src/main/java/com/SpartanJam/CSC340/repository/SongRepository.java
@@ -8,9 +8,12 @@ package com.SpartanJam.CSC340.repository;
 
 
 import com.SpartanJam.CSC340.model.ArtistSong;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SongRepository extends CrudRepository<ArtistSong, Long> 
 {
+
+    public List<ArtistSong> findByUploaderUsername(String username);
 
 }
